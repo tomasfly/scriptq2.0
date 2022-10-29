efce08bc1a628e71b932b8b3133fbfd9107af60b
Tomas <tomas@MacBook-Pro-van-Tomas.local>
2022-10-29 18:57:35 +0200

    update branch

--- forms/contact.php
+++ forms/contact.php
@@ -1,41 +1,27 @@
-<?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
+<?php 
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
-
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
-
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
-
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
-
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
-
-  echo $contact->send();
+$mail_to_send_to = "tomas.fleiderman@gmail.com";
+$from_email = "hello@scriptquality.com";
+// $sendflag = $_REQUEST['sendflag'];
+// print('print');
+// print_r('print_r');
+// print_r($_REQUEST);    
+$name=$_REQUEST['name'];
+$sendflag = "send";
+if ( $sendflag == "send" )
+        {
+                $subject= "Message subject";
+                $email = $_REQUEST['email'] ;
+                $message= "\r\n" . "Name: $name Email: $email" . "\r\n"; //get recipient name in contact form
+                $message = $message.$_REQUEST['message'] . "\r\n" ;//add message from the contact form to existing message(name of the client)
+                $headers = "From: $from_email" . "\r\n" . "Reply-To: $email"  ;
+                $a = mail( $mail_to_send_to, $subject, $message, $headers );
+                error_log("sdffds");
+                if ($a)
+                {
+                     print("OK");
+                } else {
+                     print("There was an error with your e-mail.");
+                }
+        }
 ?>
